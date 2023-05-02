faffb44d96d021c3685e0f3149db3c7054c86202
Miecznik <45920036+Miecznik@users.noreply.github.com>
2023-05-02 12:56:59 +0200

    Nauka JS w Django Project

--- Koty/static/naukaJS.js
+++ Koty/static/naukaJS.js
@@ -626,3 +626,20 @@ console.log(checker(2,3));
 console.log(checker(0,10));
 
 
+
+//41. Write a JavaScript program to check a set of three numbers; if the three numbers are the same return 30; otherwise return 20; and if two numbers are the same return 40.  Go to the editor
+
+function three_numbers(x,y,z) {
+
+    if (x == y && y == z) { return 30;}
+    else if (x == y || y == z || z == x) { return 40;}
+    else return 20;
+
+}
+console.log("Zadanie 41");
+console.log(three_numbers(10,10,10));
+console.log(three_numbers(10,10,10));
+console.log(three_numbers(10,10,10));
+
+
+
